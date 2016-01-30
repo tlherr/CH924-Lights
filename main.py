097633636a26f65829affc6d144ca37619f8db28
Thomas Herr <thom1810@gmail.com>
2016-01-30 17:28:55 -0500

    Added a helper to check versioning

--- main.py
+++ main.py
@@ -3,6 +3,9 @@
 
 from LcdManager import LcdManager
 import RPi.GPIO as GPIO
+import time
+import signal
+import sys
 
 
 PIN_COIN_INTERRUPT = 7
@@ -16,6 +19,23 @@ def main():
     GPIO.setup(PIN_COIN_INTERRUPT,GPIO.IN)
     GPIO.add_event_detect(PIN_COIN_INTERRUPT,GPIO.FALLING,callback=coinEventHandler)
 
+    signal.signal(signal.SIGINT, signal_handler)	# SIGINT = interrupt by CTRL-C
+
+    while True:
+        time.sleep(0.5)
+
+
+def signal_handler(signal, frame):
+    print('You pressed Ctrl+C, so exiting')
+    GPIO.cleanup()
+    sys.exit(0)
+
 
 def coinEventHandler (pin):
     print pin
+
+
+
+
+if __name__=="__main__":
+    main()
