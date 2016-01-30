17c4d43ca38477225de93bc8bcfe282113303717
Thomas Herr <thom1810@gmail.com>
2016-01-30 13:21:26 -0500

    Adding a base main class

---
+++ main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+
+import LcdManager
+import RPi.GPIO as GPIO
+
+
+PIN_COIN_INTERRUPT = 7
+
+
+def main():
+
+    GPIO.setmode(GPIO.BCM)
+
+    ## Setup coin interrupt channel
+    GPIO.setup(PIN_COIN_INTERRUPT,GPIO.IN)
+    GPIO.add_event_detect(PIN_COIN_INTERRUPT,GPIO.FALLING,callback=coinEventHandler)
+
+
+def coinEventHandler (pin):
+    print pin
