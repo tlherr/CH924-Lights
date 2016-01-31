2c13281c4127600cd4d1536a458f9b916d7999d1
Thomas Herr <thom1810@gmail.com>
2016-01-30 21:04:22 -0500

    Fixing runtime error

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def main():
     GPIO.setmode(GPIO.BCM)
 
     ## Setup coin interrupt channel
-    GPIO.setup(PIN_COIN_INTERRUPT,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(PIN_COIN_INTERRUPT, GPIO.IN)
     GPIO.add_event_detect(PIN_COIN_INTERRUPT, GPIO.RISING, callback=coinEventHandler)
     #GPIO.add_event_detect(PIN_COIN_INTERRUPT, GPIO.FALLING, callback=coinEventHandler)
 
