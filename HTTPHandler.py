a387e0cf81d1de897cdd02ab4300d159563a9168
Thomas Herr <thom1810@gmail.com>
2016-02-01 18:18:37 -0500

    Testing global vars

--- HTTPHandler.py
+++ HTTPHandler.py
@@ -1,6 +1,8 @@
 import BaseHTTPServer
 
 class HTTPHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    global pulses
+    global cash
     def do_HEAD(s):
         s.send_response(200)
         s.send_header("Content-type", "text/html")
@@ -11,6 +13,8 @@ class HTTPHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         s.send_header("Content-type", "text/html")
         s.end_headers()
         s.wfile.write("<html><head><title>Title goes here.</title></head>")
-        s.wfile.write("<body><p>This is a test.</p>")
+        s.wfile.write("<body>")
+        s.wfile.write("<p>Cash: {0}</p>".format(cash))
+        s.wfile.write("<p>Pulses: {0}</p>".format(pulses))
         s.wfile.write("<p>You accessed path: %s</p>" % s.path)
         s.wfile.write("</body></html>")
