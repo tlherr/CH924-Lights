918d490bab7bd93e5c4e9c8cd00d25e104ad24a5
Thomas Herr <thom1810@gmail.com>
2016-02-02 11:24:07 -0500

    Using new version of Adafruit_CharLCD library, added dependencies required to run it into dependencies shell script

--- dependencies.sh
+++ dependencies.sh
@@ -6,5 +6,8 @@ echo "Installing Build Tools"
 sudo apt-get install build-essential python-dev python-smbus python-pip
 echo "Installing Python Dependencies"
 sudo pip install RPi.GPIO
+git clone https://github.com/adafruit/Adafruit_Python_CharLCD.git
+cd Adafruit_Python_CharLCD
+sudo python setup.py install
 echo "Reconfiguring Locales. Please enable en_US.utf8"
 sudo dpkg-reconfigure locales
