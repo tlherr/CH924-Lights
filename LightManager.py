132872fa5697b703233714ab30f21c3a7312b3ff
Thomas Herr <thom1810@gmail.com>
2016-02-11 14:55:28 -0500

    Reverting update functionality after testing

--- LightManager.py
+++ LightManager.py
@@ -98,7 +98,7 @@ class LightManager:
             if self.override:
                 # print("Light Manager Override Detected, Light On")
                 GPIO.output(self.PIN_LIGHT, True)
-            elif self.time_remaining is not None and self.override is not False:
+            elif self.time_remaining is not None:
                 if self.time_remaining > 0:
                     # print("Light Manager Active Time Detected")
                     GPIO.output(self.PIN_LIGHT, True)
