e6cf94bd0903bbcdcb5bd8671b70e238f59cf704
Thomas Herr <thom1810@gmail.com>
2016-03-08 15:40:22 -0500

    Lowering bounce time

--- CoinMachineManager.py
+++ CoinMachineManager.py
@@ -33,7 +33,7 @@ class CoinMachineManager:
         # Setup coin interrupt channel
         print("Setting Pin: {0} to Input mode, pulled down".format(self.PIN_COIN_INTERRUPT))
         GPIO.setup(self.PIN_COIN_INTERRUPT, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-        GPIO.add_event_detect(self.PIN_COIN_INTERRUPT, GPIO.RISING, callback=self.coin_event_handler, bouncetime=500)
+        GPIO.add_event_detect(self.PIN_COIN_INTERRUPT, GPIO.RISING, callback=self.coin_event_handler, bouncetime=100)
 
     def set_price_per_hour(self, rate):
         self.price_per_hour = rate
